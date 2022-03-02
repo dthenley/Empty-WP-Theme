e154197c5506213279a9677b44354ae2fb6c54b1
Donte Henley <donte@dontehenley.com>
2022-03-02 16:17:46 -0600

    Breaking out the index

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    <a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', '_empty-wp-theme' ); ?></a>
+
+    <header>
+        <nav></nav>
+    </header>
--- index.php
+++ index.php
@@ -1,22 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php wp_head(); ?>
-</head>
-<body <?php body_class(); ?>>
-    <a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', '_empty-wp-theme' ); ?></a>
+<?php
 
-    <header>
-        <nav></nav>
-    </header>
+get_header();
+?>
     
     <main id="primary">
-        <?php the_content(); ?>
+        <?php 
+        if ( have_posts() ) : 
+            while ( have_posts() ) : the_post(); 
+                the_content();
+            endwhile; 
+        else: 
+            _e( 'Sorry, no pages matched your criteria.', 'empty-wp-theme' ); 
+        endif; 
+        ?>
     </main>
 
-    <footer></footer>
-</body>
-</html>
+<?php
+get_sidebar();
+get_footer();
