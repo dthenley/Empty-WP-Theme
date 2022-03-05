48e3cc76a24ccb67527ee4505d3949390977b72c
Donte Henley <donte@dontehenley.com>
2022-03-05 16:32:31 -0600

    starting on navigation css

--- header.php
+++ header.php
@@ -17,8 +17,9 @@
             <?php
             wp_nav_menu(
                 array(
-                    'theme_location' => 'primary',
-                    'menu_id'        => 'primary-menu',
+                    'theme_location'  => 'primary',
+                    'menu_id'         => 'primary-menu',
+                    'container_class' => 'primary-menu-container',
                     
                 )
             );
