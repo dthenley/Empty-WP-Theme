0328473bbc7c5eb0a3487c21ee24e1113e1a2707
Donte Henley <donte@dontehenley.com>
2022-03-07 13:14:53 -0600

    Add header functionality and styling

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -1,5 +1,8 @@
-var toggle = document.querySelector('#menu-toggle');
-var menu = document.querySelector('.menu-primary-container');
+/**
+ * Action when clicking menu-toggle on mobile menu
+ */
+const toggle = document.querySelector('#menu-toggle');
+const menu = document.querySelector('.menu-primary-container');
 
 toggle.addEventListener('click', function(){
   if (menu.classList.contains('expand')) {
@@ -10,3 +13,18 @@ toggle.addEventListener('click', function(){
     this.setAttribute('aria-expanded', 'true');
   }
 });
+
+/**
+ * Checks for class with menu-item-has-children and add button
+ */
+let dropdownMenuItem = document.querySelectorAll('.menu-item-has-children');
+let dropdownButton = '<button class="dropdown-button">+</button>';
+
+dropdownMenuItem.forEach(dropDownElement => { dropDownElement.innerHTML += dropdownButton })
+
+
+document.querySelectorAll('.dropdown-button').forEach(dropdownButtonElement => {
+  dropdownButtonElement.addEventListener('click', function(){
+    this.classList.toggle('expand');
+  })
+})
